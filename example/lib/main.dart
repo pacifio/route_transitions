987e9391c1151454cfbc14d778b669071e5b0274
Adib Mohsin <adibmohsin.root@gmail.com>
2021-07-28 20:38:49 +0600

    implemeting null safety

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:route_transitions/route_transitions.dart';
+// import 'package:route_transitions/route_transitions.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,183 +9,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      title: 'Route transitions',
+      theme: ThemeData.dark(),
       home: Home(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class Home extends StatefulWidget {
-  Home({Key key}) : super(key: key);
-
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        elevation: 8.0,
-        title: Text(
-          "Route animations",
-          style: style,
-        ),
-        brightness: Brightness.dark,
-      ),
-      body: Container(
-        margin: EdgeInsets.symmetric(horizontal: 50),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                elevation: 0.0,
-                primary: Colors.blue,
-              ),
-              child: Center(
-                child: Text(
-                  "Slide from right",
-                  style: style.copyWith(color: Colors.white),
-                ),
-              ),
-              onPressed: () {
-                Navigator.of(context).push(PageRouteTransition(
-                    animationType: AnimationType.slide_right,
-                    builder: (context) => DashBoard()));
-              },
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                elevation: 0.0,
-                primary: Colors.blue,
-              ),
-              child: Center(
-                child: Text(
-                  "Slide from left",
-                  style: style.copyWith(color: Colors.white),
-                ),
-              ),
-              onPressed: () {
-                Navigator.of(context).push(PageRouteTransition(
-                    animationType: AnimationType.slide_left,
-                    builder: (context) => DashBoard()));
-              },
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                elevation: 0.0,
-                primary: Colors.blue,
-              ),
-              child: Center(
-                child: Text(
-                  "Slide from up",
-                  style: style.copyWith(color: Colors.white),
-                ),
-              ),
-              onPressed: () {
-                Navigator.of(context).push(PageRouteTransition(
-                    animationType: AnimationType.slide_up,
-                    builder: (context) => DashBoard()));
-              },
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                elevation: 0.0,
-                primary: Colors.blue,
-              ),
-              child: Center(
-                child: Text(
-                  "Slide from down",
-                  style: style.copyWith(color: Colors.white),
-                ),
-              ),
-              onPressed: () {
-                Navigator.of(context).push(PageRouteTransition(
-                    animationType: AnimationType.slide_down,
-                    builder: (context) => DashBoard()));
-              },
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                elevation: 0.0,
-                primary: Colors.blue,
-              ),
-              child: Center(
-                child: Text(
-                  "Fade",
-                  style: style.copyWith(color: Colors.white),
-                ),
-              ),
-              onPressed: () {
-                Navigator.of(context).push(PageRouteTransition(
-                    animationType: AnimationType.fade,
-                    builder: (context) => DashBoard()));
-              },
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                elevation: 0.0,
-                primary: Colors.blue,
-              ),
-              child: Center(
-                child: Text(
-                  "Scale",
-                  style: style.copyWith(color: Colors.white),
-                ),
-              ),
-              onPressed: () {
-                Navigator.of(context).push(PageRouteTransition(
-                    animationType: AnimationType.scale,
-                    builder: (context) => DashBoard()));
-              },
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class DashBoard extends StatelessWidget {
-  const DashBoard({Key key}) : super(key: key);
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        brightness: Brightness.dark,
-        title: Text(
-          "Dashboard",
-          style: style,
-        ),
-        backgroundColor: Colors.blue,
-        elevation: 8.0,
-        leading: IconButton(
-          icon: Icon(
-            Icons.chevron_left,
-            color: Colors.white,
-          ),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
-      ),
-      body: Center(
-        child: Text(
-          "Dashboard",
-          style: style,
-        ),
-      ),
+    return Container(
+      child: null,
     );
   }
 }
--- lib/route_transitions.dart
+++ lib/route_transitions.dart
@@ -1,132 +1,277 @@
-library route_transitions;
-
-/*
-BSD 2-Clause License
-
-Copyright (c) 2019, Adib Mohsin
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
 
-/// AnimationType defines what route transtition should take place
-enum AnimationType {
-  slide_right,
-  slide_left,
-  slide_up,
-  slide_down,
-  fade,
-  scale,
+/// Push a new widget , uses Navigator.push method
+void pushWidget(
+    {required Widget newPage,
+    required BuildContext context,
+    bool pushBackPrevPage = false}) {
+  if (pushBackPrevPage) {
+    Navigator.of(context).pop();
+  }
+  Navigator.of(context).push(MaterialPageRoute(builder: (context) => newPage));
 }
 
-/// The main class that defines a custom Route transition / animation
-/// [WidgetBuilder builder] is required
-/// [Curves curves] is optional , by default it is set to [Curves.easeInOut]
-class PageRouteTransition extends MaterialPageRoute {
-  AnimationType animationType;
-  Cubic curves;
-
-  PageRouteTransition(
-      {@required WidgetBuilder builder,
-      RouteSettings settings,
-      AnimationType animationType = AnimationType.slide_right,
-      Cubic curves = Curves.easeInOut,
-      bool maintainState = true,
-      bool fullscreenDialog = false})
-      : animationType = animationType,
-        curves = curves,
-        super(
-            builder: builder,
-            settings: settings,
-            maintainState: maintainState,
-            fullscreenDialog: fullscreenDialog);
-
-  @override
-  Widget buildTransitions(BuildContext context, Animation<double> animation,
-      Animation<double> secondaryAnimation, Widget child) {
-    Animation customAnimation;
-    if (animationType == AnimationType.slide_right) {
-      customAnimation = Tween(begin: Offset(1.0, 0.0), end: Offset(0.0, 0.0))
-          .animate(CurvedAnimation(
-        parent: animation,
-        curve: curves,
-      ));
-      return SlideTransition(
-        position: customAnimation,
-        child: child,
-      );
-    } else if (animationType == AnimationType.slide_left) {
-      customAnimation = Tween(begin: Offset(-1.0, 0.0), end: Offset(0.0, 0.0))
-          .animate(CurvedAnimation(
-        parent: animation,
-        curve: curves,
-      ));
-      return SlideTransition(
-        position: customAnimation,
-        child: child,
-      );
-    } else if (animationType == AnimationType.slide_up) {
-      customAnimation = Tween(begin: Offset(0.0, 1.0), end: Offset(0.0, 0.0))
-          .animate(CurvedAnimation(
-        parent: animation,
-        curve: curves,
-      ));
-      return SlideTransition(
-        position: customAnimation,
-        child: child,
-      );
-    } else if (animationType == AnimationType.slide_down) {
-      customAnimation = Tween(begin: Offset(0.0, -1.0), end: Offset(0.0, 0.0))
-          .animate(CurvedAnimation(
-        parent: animation,
-        curve: curves,
-      ));
-      return SlideTransition(
-        position: customAnimation,
-        child: child,
-      );
-    } else if (animationType == AnimationType.fade) {
-      customAnimation = Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
-        parent: animation,
-        curve: curves,
-      ));
-      return FadeTransition(
-        opacity: customAnimation,
-        child: child,
-      );
-    } else if (animationType == AnimationType.scale) {
-      customAnimation = Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
-        parent: animation,
-        curve: curves,
-      ));
-      return ScaleTransition(
-        scale: customAnimation,
-        child: child,
-      );
-    } else {
-      throw new Exception("Animation type is invalid or null");
-    }
+/// Push a named route , uses Navigator.push method
+void pushNamed(
+    {required String newPage,
+    required BuildContext context,
+    bool pushBackPrevPage = false}) {
+  if (pushBackPrevPage) {
+    Navigator.of(context).pop();
   }
+  Navigator.of(context).pushNamed(newPage);
+}
+
+/// Push a widget while removing all previous routes
+void pushWidgetWhileRemove(
+    {required Widget newPage,
+    required BuildContext context,
+    bool pushBackPrevPage = false}) {
+  if (pushBackPrevPage) {
+    Navigator.of(context).pop();
+  }
+  Navigator.of(context).pushAndRemoveUntil(
+      MaterialPageRoute(builder: (context) => newPage), (route) => false);
+}
+
+/// Push a named route while removing all previous routes
+void pushNamedWhileRemove(
+    {required String newPage,
+    required BuildContext context,
+    bool pushBackPrevPage = false}) {
+  if (pushBackPrevPage) {
+    Navigator.of(context).pop();
+  }
+  Navigator.of(context).pushNamedAndRemoveUntil(newPage, (route) => false);
+}
+
+/// Push a widget and await for data returned from `Navigator.pop()`
+Future<dynamic> pushWidgetAwait(
+    {required Widget newPage,
+    required BuildContext context,
+    bool pushBackPrevPage = false}) async {
+  if (pushBackPrevPage) {
+    Navigator.of(context).pop();
+  }
+  return Future.value(await Navigator.of(context)
+      .push(MaterialPageRoute(builder: (context) => newPage)));
+}
+
+/// Push a named route and await for data returned from `Navigator.pop()`
+Future<dynamic> pushNamedAwait(
+    {required String newPage,
+    required BuildContext context,
+    bool pushBackPrevPage = false}) async {
+  if (pushBackPrevPage) {
+    Navigator.of(context).pop();
+  }
+  return Future.value(await Navigator.of(context).pushNamed(newPage));
+}
+
+/// Replcae with a new widget , uses `Navigator.pushReplacement()` method
+void replaceWidget({required Widget newPage, required BuildContext context}) {
+  Navigator.of(context)
+      .pushReplacement(MaterialPageRoute(builder: (context) => newPage));
+}
+
+/// Replcae with a named route , uses `Navigator.pushReplacement()` method
+void replaceNamed({required String newPage, required BuildContext context}) {
+  Navigator.of(context).pushReplacementNamed(newPage);
+}
+
+/// Custom page animation with fade transition
+Future fadeWidget({
+  required Widget newPage,
+  required BuildContext context,
+  bool opaque = true,
+}) {
+  return Navigator.of(context).push(PageRouteBuilder(
+    opaque: opaque,
+    pageBuilder: (context, animation, secondaryAnimation) {
+      return newPage;
+    },
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      var begin = 0.0;
+      var end = 1.0;
+      var curve = Curves.easeIn;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return FadeTransition(
+        opacity: animation.drive(tween),
+        child: child,
+      );
+    },
+  ));
+}
+
+/// Custom page animation with new page sliding in from right side
+Future slideRightWidget({
+  required Widget newPage,
+  required BuildContext context,
+  bool opaque = true,
+}) {
+  return Navigator.of(context).push(PageRouteBuilder(
+    opaque: opaque,
+    pageBuilder: (context, animation, secondaryAnimation) {
+      return newPage;
+    },
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      var begin = Offset(1.0, 0.0);
+      var end = Offset(0.0, 0.0);
+      var curve = Curves.easeIn;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
+  ));
+}
+
+/// Custom page animation with new page sliding in from lef side
+Future slideLeftWidget({
+  required Widget newPage,
+  required BuildContext context,
+  bool opaque = true,
+}) {
+  return Navigator.of(context).push(PageRouteBuilder(
+    opaque: opaque,
+    pageBuilder: (context, animation, secondaryAnimation) {
+      return newPage;
+    },
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      var begin = Offset(-1.0, 0.0);
+      var end = Offset(0.0, 0.0);
+      var curve = Curves.easeIn;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
+  ));
+}
+
+/// Custom page animation with new page sliding up
+Future slideUpWidget({
+  required Widget newPage,
+  required BuildContext context,
+  bool opaque = true,
+}) {
+  return Navigator.of(context).push(PageRouteBuilder(
+    opaque: opaque,
+    pageBuilder: (context, animation, secondaryAnimation) {
+      return newPage;
+    },
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      var begin = Offset(0.0, 1.0);
+      var end = Offset(0.0, 0.0);
+      var curve = Curves.easeIn;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
+  ));
+}
+
+/// Custom page animation with new page sliding down
+Future slideDownWidget({
+  required Widget newPage,
+  required BuildContext context,
+  bool opaque = true,
+}) {
+  return Navigator.of(context).push(PageRouteBuilder(
+    opaque: opaque,
+    pageBuilder: (context, animation, secondaryAnimation) {
+      return newPage;
+    },
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      var begin = Offset(0.0, -1.0);
+      var end = Offset(0.0, 0.0);
+      var curve = Curves.easeIn;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
+  ));
+}
+
+/// Custom page animation with new page sliding down
+Future scaleWidget({
+  required Widget newPage,
+  required BuildContext context,
+  bool opaque = true,
+}) {
+  return Navigator.of(context).push(PageRouteBuilder(
+    opaque: opaque,
+    pageBuilder: (context, animation, secondaryAnimation) {
+      return newPage;
+    },
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      var begin = 0.0;
+      var end = 1.0;
+      var curve = Curves.easeIn;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return ScaleTransition(
+        scale: animation.drive(tween),
+        child: child,
+      );
+    },
+  ));
+}
+
+/// Create a custom animation with [transitionBuilder] parameter
+///
+/// Here is an example of Scale Transition
+/// ```
+/// (context, animation, secondaryAnimation, child) {
+///   var begin = 0.0;
+///   var end = 1.0;
+///   var curve = Curves.easeIn;
+///   var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+///   return ScaleTransition(
+///     scale: animation.drive(tween),
+///     child: child,
+///   );
+/// }
+/// ```
+Future customAnimationWidget({
+  required Widget newPage,
+  required BuildContext context,
+  required Widget Function(
+    BuildContext,
+    Animation<double>,
+    Animation<double>,
+    Widget,
+  )
+      transitionBuilder,
+  bool opaque = true,
+}) {
+  return Navigator.of(context).push(PageRouteBuilder(
+    opaque: opaque,
+    pageBuilder: (context, animation, secondaryAnimation) {
+      return newPage;
+    },
+    transitionsBuilder: transitionBuilder,
+  ));
+}
+
+/// Pop widget from navigation tree
+void pop(BuildContext context) {
+  Navigator.of(context).pop();
 }
